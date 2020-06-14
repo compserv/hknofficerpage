a08759bd4eea63cb9ed7c5a7353b72f10a1f1051
Danny Chu <dannychu@berkeley.edu>
2020-06-13 17:23:15 -0700

    Deploy gh-pages

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2309420da77ce4c7e8f8";
+/******/ 	var hotCurrentHash = "f8e97598f29995ab58ad";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
