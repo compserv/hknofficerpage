559465ec984197d2d765e8865b72ec3f7cd15036
Danny Chu <dannychu@berkeley.edu>
2020-06-13 17:14:50 -0700

    Revert "switched picture an profiles"
    This reverts commit 1484a5505ad7dd49ef6107a71a0a129e222f1e96.

--- components/committee.js
+++ components/committee.js
@@ -4,15 +4,14 @@ import Profile from '../components/profile'
 function Committee(props) {
 	return (
 		<section className="pa4">
-			<div className="fw9 f2 tl-l tc pa2">{props.comm.name}</div>
+			<div className="b f2 tl-l tc pa2">{props.comm.name}</div>
 			<div className="">
-				<div className="mw6-l pa1 dib-l v-top">
-					<img src={"committees/"+ props.comm.name +".jpg"} className="w-100 br2" alt="Photo of Execs" />
-				</div>
 				<div className="dib-l v-top w-60-l mw7-l pa1">
 					{props.comm.officers.map(officer => <Profile officer={officer}/>)}
 				</div>
-				
+				<div className="w-40-l pa1 dib-l v-top">
+					<img src={"committees/"+ props.comm.name +".jpg"} className="w-100 br2" alt="Photo of Execs" />
+				</div>
 				
 			</div>
 		</section>
